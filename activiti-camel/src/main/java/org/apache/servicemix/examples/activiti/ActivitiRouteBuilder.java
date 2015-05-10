8ac806679605d3355633ada805610dd2c60c11cd
Pipat Waitayaworanart <coploftbas@gmail.com>
2015-05-10 17:21:35 +0700

    Integrate BPMN20 from BPMN designer (still got some errors)

--- activiti-camel/src/main/java/org/apache/servicemix/examples/activiti/ActivitiRouteBuilder.java
+++ activiti-camel/src/main/java/org/apache/servicemix/examples/activiti/ActivitiRouteBuilder.java
@@ -46,32 +46,35 @@ public class ActivitiRouteBuilder extends RouteBuilder {
         from("file:var/activiti-camel/order")
             .setBody(bean(helper))
             .setProperty(PROCESS_KEY_PROPERTY, simple("file:name"))
-            .to("activiti:OrderProcess")
-            .log("Process to handle incoming order file has been started (process instance id ${body})");
+            .log("Process to handle incoming order file has been started (process instance id ${body})")
+            .to("activiti:OrderProcess");
         
         /*
          * This route will notify a running OrderProcess of an order delivery event.  Here too, we are setting the
          * PROCESS_KEY_PROPERTY to correlate the delivery message with right order process instance.
          */
-        from("file:var/activiti-camel/delivery")
+        /*from("file:var/activiti-camel/delivery")
             .log("Notifying process about delivery for order ${file:name}")
             .setBody(bean(helper))
             .setProperty(PROCESS_KEY_PROPERTY, simple("file:name"))
             .to("activiti:OrderProcess:receiveDelivery");
-
+        */
         /*
          * The BPMN process can also trigger Camel routes as part of the process.  In these routes, the variables that
          * you added to the process are available as Exchange properties.  The next two routes will be triggered while
          * processing the order and the order delivery.
          */
-        from("activiti:OrderProcess:processOrder?copyVariablesToProperties=true")
-            .log("Processing order ${property.orderid} created on ${property.timestamp}")
+        from("activiti:OrderProcess:getViolatedEvidenceProcess?copyVariablesToProperties=true")
+            .log("Processing evidence ${property.orderid} created on ${property.timestamp}")
             .log("  original message: ${property.message}");
+            //.setBody(bean(helper))
+            //.setProperty(PROCESS_KEY_PROPERTY, simple("${property}"))
+            //.to("activiti:OrderProcess:validateLicensePlateProcess");
 
-        from("activiti:OrderProcess:processDelivery?copyVariablesToProperties=true")
+        /*from("activiti:OrderProcess:processDelivery?copyVariablesToProperties=true")
             .log("Processing delivery for order ${property.orderid} created on ${property.timestamp}")
             .log("  original message: ${property.message}");
-
+        */
     }
 
     /*
